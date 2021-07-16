7239b5aae2eed9ce28535989aa1caacf0fd01d0a
Nataly <natulya.lukashina@bk.ru>
2021-07-16 18:46:29 +0300

    corrected telegram file

--- source/phpmailer/telegram.php
+++ source/phpmailer/telegram.php
@@ -1,28 +1,34 @@
 <?php
 
-/* https://api.telegram.org/botXXXXXXXXXXXXXXXXXXXXXXX/getUpdates,
-где, XXXXXXXXXXXXXXXXXXXXXXX - токен вашего бота, полученный ранее */
+  /* https://api.telegram.org/bot:XXXXXXXXXXXXXXXXXXXXXXX/getUpdates,
+  где, XXXXXXXXXXXXXXXXXXXXXXX - токен вашего бота, полученный ранее */
 
-$name = $_POST['user_name'];
-$phone = $_POST['user_phone'];
-$email = $_POST['user_email'];
-$token = "";
-$chat_id = "";
-$arr = array(
-  'Имя пользователя: ' => $name,
-  'Телефон: ' => $phone,
-  'Email' => $email
-);
+  $name = $_POST['name'];
+  $phone = $_POST['phone'];
+  $time = $_POST['time'];
+  $token = "12321231232123123212312321231232123";
+  $chat_id = "55555555";
+  $arr = array(
+    'Имя пользователя: ' => $name,
+    'Телефон: ' => $phone,
+    'Удобное время для звонка' => $time
+  );
 
-foreach($arr as $key => $value) {
-  $txt .= "<b>".$key."</b> ".$value."%0A";
-};
+  foreach($arr as $key => $value) {
+    $txt .= "<b>".$key."</b> ".$value."%0A";
+  };
 
-$sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
+  $sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
 
-if ($sendToTelegram) {
-  header('Location: thank-you.html');
-} else {
-  header('Location: error.html');
-}
+  // if ($sendToTelegram) {
+  //   // header('Location: thank-you.html');
+  //   $message = "Error";
+  // } else {
+  //   // echo "Error";
+  //   $message = "Success";
+  // }
+
+  $response = ['message' => $message];
+  header("Content-type: application/json");
+  echo json_encode($response);
 ?>
